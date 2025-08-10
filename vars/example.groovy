ead28271ac5e3543bccd0218c79a3d761afe95e2
Ulises Lárraga <uliseslarraga@gmail.com>
2025-08-09 21:25:12 -0600

    Adding sample pipeline

---
+++ vars/example.groovy
@@ -0,0 +1,12 @@
+def call(){
+    pipeline {
+        agent any
+        stages {
+            stage('Example') {
+                steps {
+                    echo 'Hello, World!'
+                }
+            }
+        }
+    }
+}
